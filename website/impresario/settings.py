5667f4188b1fd4e2589ccd56ed3e0ef65dc2cecb
diya-gupta29 <goyaldiya55@gmail.com>
2020-12-03 22:31:08 +0530

    Added models

--- website/impresario/settings.py
+++ website/impresario/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'userauth.apps.UserauthConfig'
+    'userauth.apps.UserauthConfig',
+    'scheduling.apps.SchedulingConfig',
+
 ]
 
 MIDDLEWARE = [
